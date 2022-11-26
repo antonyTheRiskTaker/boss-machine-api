57e54bfe32c14d350dbd4caecb23b22c57c80712
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-26 17:35:17 +0800

    server entry point set up

--- server/api.js
+++ server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
+const db = require('./db');
 
-
+apiRouter.get('/minions', (req, res, next) => {
+  const minions = db.getAllFromDatabase('minions');
+  res.status(200).send(minions);
+});
 
 module.exports = apiRouter;
