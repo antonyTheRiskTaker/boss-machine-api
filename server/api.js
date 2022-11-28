a27535904369b168b5b0de6b77babe1282571a91
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-28 10:35:25 +0800

    working on handling post route for minions

--- server/api.js
+++ server/api.js
@@ -2,9 +2,15 @@ const express = require('express');
 const apiRouter = express.Router();
 const db = require('./db');
 
+// Get an array of all minions.
 apiRouter.get('/minions', (req, res, next) => {
   const minions = db.getAllFromDatabase('minions');
   res.status(200).send(minions);
+});
+
+// Create a new minion and save it to the database.
+apiRouter.post('/minions', (req, res, next) => {
+  
 });
 
 module.exports = apiRouter;
