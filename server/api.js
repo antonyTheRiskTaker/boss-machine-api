09b2a2d617e527361fef3e97ab898e2a2ce7491d
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-29 10:19:42 +0800

    minion post route finished

--- server/api.js
+++ server/api.js
@@ -10,7 +10,12 @@ apiRouter.get('/minions', (req, res, next) => {
 
 // Create a new minion and save it to the database.
 apiRouter.post('/minions', (req, res, next) => {
-  
+  const newMinion = req.body;
+  const result = db.addToDatabase('minions', newMinion);
+  res.status(201).send(result);
 });
+
+// Get a single minion by id.
+// TODO: continue from here
 
 module.exports = apiRouter;
