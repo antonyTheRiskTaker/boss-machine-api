b24029f2e5b39a77e7301186c4d42a6db37d2e5d
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-30 10:28:57 +0800

    route retrieving single minion done & wrote middleware to extract id

--- server/api.js
+++ server/api.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
 const db = require('./db');
+
+// Middleware to extract the minionId parameter for further processing.
+apiRouter.param('minionId', (req, res, next, id) => {
+  req.minionId = id;
+  next();
+});
 
 // Get an array of all minions.
 apiRouter.get('/minions', (req, res, next) => {
@@ -16,6 +23,14 @@ apiRouter.post('/minions', (req, res, next) => {
 });
 
 // Get a single minion by id.
-// TODO: continue from here
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+  const minionId = req.minionId;
+  const foundMinion = db.getFromDatabaseById('minions', minionId);
+  if (foundMinion) {
+    res.status(200).send(foundMinion);
+  } else {
+    res.status(404).send('Cannot find anyone with this ID.');
+  }
+});
 
 module.exports = apiRouter;
