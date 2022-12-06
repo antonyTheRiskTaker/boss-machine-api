d8d2103eee16e258a2922798c007eb46dc2da465
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-06 12:29:56 +0800

    idea revenue checker function done

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,9 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+  const convertedWeeks = Number(numWeeks);
+  const convertedWeeklyRev = Number(weeklyRevenue)
+  const totalRevenue = convertedWeeklyRev * convertedWeeks;
+  return totalRevenue >= 1000000;
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
