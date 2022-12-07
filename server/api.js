1506544e0f96fb259e8850605f17d5fbce672458
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-07 09:35:31 +0800

    created a separate file to handle routes for meetings

--- server/api.js
+++ server/api.js
@@ -2,52 +2,12 @@ const express = require('express');
 const app = require('../server');
 const apiRouter = express.Router();
 
-const {
-  getAllFromDatabase,
-  addToDatabase,
-  getFromDatabaseById,
-  updateInstanceInDatabase,
-  deleteFromDatabasebyId,
-  deleteAllFromDatabase,
-  createMeeting
-} = require('./db');
-
-// Database model type constants.
-const MEETINGS = 'meetings';
-
 // Require routers for specific collections of resources
 const minionsRouter = require('./minionsRouter');
 const ideasRouter = require('./ideasRouter');
+const meetingsRouter = require('./meetingsRouter');
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
-
-// Get an array of all meetings.
-apiRouter.get('/meetings', (req, res, next) => {
-  const meetings = getAllFromDatabase(MEETINGS);
-
-  if (meetings !== null) {
-    res.status(200).send(meetings);
-  } else {
-    res.status(500).send();
-  }
-});
-
-// Create a new meeting and save it to the database.
-apiRouter.post('/meetings', (req, res, next) => {
-  const newMeeting = createMeeting();
-  const result = addToDatabase(MEETINGS, newMeeting);
-  res.status(200).send(result);
-});
-
-// Delete all meetings from the database.
-apiRouter.delete('/meetings', (req, res, next) => {
-  const result = deleteAllFromDatabase(MEETINGS);
-  
-  if (result !== null) {
-    res.status(204).send(result);
-  } else {
-    res.status(500).send();
-  }
-});
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
---
+++ server/meetingsRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const meetingsRouter = express.Router();
+
+const {
+  getAllFromDatabase,
+  addToDatabase,
+  deleteAllFromDatabase,
+  createMeeting
+} = require('./db');
+
+// Database model type constant for meetings.
+const MEETINGS = 'meetings';
+
+// Get an array of all meetings.
+meetingsRouter.get('/', (req, res, next) => {
+  const meetings = getAllFromDatabase(MEETINGS);
+
+  if (meetings !== null) {
+    res.status(200).send(meetings);
+  } else {
+    res.status(500).send();
+  }
+});
+
+// Create a new meeting and save it to the database.
+meetingsRouter.post('/', (req, res, next) => {
+  const newMeeting = createMeeting();
+  const result = addToDatabase(MEETINGS, newMeeting);
+  res.status(200).send(result);
+});
+
+// Delete all meetings from the database.
+meetingsRouter.delete('/', (req, res, next) => {
+  const result = deleteAllFromDatabase(MEETINGS);
+  
+  if (result !== null) {
+    res.status(204).send(result);
+  } else {
+    res.status(500).send();
+  }
+});
+
+module.exports = meetingsRouter;
